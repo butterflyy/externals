005d8f42381b6b50d9fdd8876989d969b4bd9e44
liuyongang <liuyongang-jj@qq.com>
2019-07-24 00:05:56 +0800

    Add set bk colro and button st.

--- include/MFC/HelperDlg.h
+++ include/MFC/HelperDlg.h
@@ -3,12 +3,27 @@
 #include <afxwin.h>
 #include <common\utils.h>
 
+#define SET_BTST(shadeButtons, buttonSize, bkColor) \
+for (int i = 0; i < buttonSize; i++){ \
+	shadeButtons[i].SetShade(CShadeButtonST::SHS_HSHADE, 4, 20, 15, bkColor); \
+}\
+
+#define SET_DEFAULT_BTST(shadeButtons, buttonSize) SET_BTST(shadeButtons, buttonSize, RGB(91, 130, 180))
+
 class HelperDlg{
 protected:
-	HelperDlg(CDialog* dlg, bool en= false):
+	HelperDlg(CDialog* dlg, 
+		COLORREF bkColor = RGB(255, 255, 255),
+		int bts = 0,
+		bool en= false
+		):
 		m_dlg(dlg),
-		m_en(en){
+		m_en(en),
+		m_bkColor(bkColor){
+		m_brush.CreateSolidBrush(bkColor);
 	}
+
+
 
 	void ShowMessage(const std::string& msg){
 		if (m_en){
@@ -37,7 +52,22 @@ protected:
 		return utils::StrToInt(GetDlgText(id));
 	}
 
+	void ShowItem(int id){
+		::ShowWindow(::GetDlgItem(m_dlg->GetSafeHwnd(), id), SW_SHOWNORMAL);
+	}
+
+	void HideItem(int id){
+		::ShowWindow(::GetDlgItem(m_dlg->GetSafeHwnd(), id), SW_HIDE);
+	}
+
+	//interface
+	HBRUSH SetCtlColor(CDC* pDC){
+		pDC->SetBkColor(m_bkColor);
+		return m_brush;
+	}
 protected:
 	CDialog* m_dlg;
 	bool m_en;
+	CBrush m_brush;
+	COLORREF m_bkColor;
 };
