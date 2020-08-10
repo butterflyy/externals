5395bc07d8ba7e6b08445481f07e29b1ea44b213
butterfly <liuyonggang-jj@qq.com>
2020-08-10 14:21:00 +0800

    Update ObjectList

--- include/common/ObjectList.h
+++ include/common/ObjectList.h
@@ -1,3 +1,6 @@
+//author : lyg
+//data : 2020-8
+
 #ifndef __OBJECT_LIST_H__
 #define __OBJECT_LIST_H__
 
@@ -5,9 +8,28 @@
 #include <common/utils.h> //for mutex
 
 template<typename T>
+struct DefaultDeleter{
+	typedef T item_type;
+
+	void operator()(const item_type& item){
+		//do nothing
+	}
+};
+
+template<typename T>
+struct DefaultDeleter<T*>{
+	typedef T* item_type;
+
+	void operator()(const item_type& item){
+		if (item)
+			delete item;
+	}
+};
+
+template<typename T>
 class ObjectList{
 public:
-	typedef std::list<item> List;
+	typedef std::list<T> List;
 	typedef T item_type;
 
 	void Add(const item_type& item){
@@ -20,21 +42,25 @@ public:
 		_items.push_back(std::move(item));
 	}
 
-	void Remove(const item_type& item){
+	template<typename Deleter>
+	void Remove(const item_type& item, Deleter d){
 		utils::LockGuard<utils::Mutex> lock(_mutex);
 
 		for (List::iterator it = _items.begin();
 			it != _items.end();){
 			if (*it == item){
-				if (std::is_pointer<item_type>::value){
-					delete (*it);
-				}
+				d(*it);
 				it = _items.erase(it);
 			}
 			else{
 				it++;
 			}
 		}
+	}
+
+	//use default deleter
+	void Remove(const item_type& item){
+		Remove(item, DefaultDeleter<item_type>());
 	}
 
 	bool Has(const item_type& item){
@@ -49,19 +75,52 @@ public:
 		return false;
 	}
 
-	void Clear(){
+	template<typename Deleter>
+	void Clear(Deleter d){
 		utils::LockGuard<utils::Mutex> lock(_mutex);
-
-		if (std::is_pointer<item_type>::value){
-			for (List::const_iterator it = _items.begin();
-				it != _items.end();
-				++it){
-				delete (*it);
-			}
+		for (List::const_iterator it = _items.begin();
+			it != _items.end();
+			++it){
+			d(*it);
 		}
+
 		_items.clear();
 	}
 
+	//use default deleter
+	void Clear(){
+		Clear(DefaultDeleter<item_type>());
+	}
+
+	item_type& operator[](size_t index){
+		if (index >= _items.size()){
+			throw std::out_of_range("index big than size");
+		}
+		assert(index < _items.size());
+		int i = 0;
+		for (List::iterator it = _items.begin();
+			it != _items.end();
+			++it, ++i){
+			if (i == index){
+				return *it;
+			}
+		}
+		assert(false);
+		return *(_items.begin());
+	}
+
+	void Lock(){
+		_mutex.lock();
+	}
+
+	//return raw data, must need lock to handle data
+	List& Data(){
+		return _items;
+	}
+
+	void Unlock(){
+		_mutex.unlock();
+	}
 private:
 	List _items;
 	utils::Mutex _mutex;
