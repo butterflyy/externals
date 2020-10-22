0db965916c1de078f827a5254abd4cf5d6b6869e
butterfly <liuyonggang-jj@qq.com>
2020-10-22 16:26:17 +0800

    path.h changed

--- include/common/path.h
+++ include/common/path.h
@@ -5,34 +5,41 @@
 #include <windows.h>
 #endif
 
-
-
 namespace utils{
+	namespace path{
 #ifdef WIN32
-	static const std::string seprator = "\\";
+		static const std::string seprator = "\\";
 #else
-	static const std::string seprator = "/";
+		static const std::string seprator = "/";
 #endif
 
-	static std::string get_tmp(){
+		static std::string get_tmp(){
 #ifdef WIN32
-		char buff[MAX_PATH] = {0};
-		::GetTempPathA(MAX_PATH, buff);
-		int len = strlen(buff);
-		if (len >= 1){//remove dir
-			buff[len - 1] = 0;
+			char buff[MAX_PATH] = { 0 };
+			::GetTempPathA(MAX_PATH, buff);
+			int len = strlen(buff);
+			if (len >= 1){//remove dir
+				buff[len - 1] = 0;
+			}
+			return std::string(buff);
+#else
+			return "/tmp";
+#endif
 		}
-		return std::string(buff);
-#else
-		return "/tmp";
-#endif
-	}
 
-	//Append to path, return path.
-	static std::string& append(std::string& path, const std::string& name){
-		path.append(seprator);
-		path.append(name);
-		return path;
+		//Append to path, return path.
+		static std::string& append(std::string& dir, const std::string& name){
+			dir.append(seprator);
+			dir.append(name);
+			return dir;
+		}
+
+		static std::string add(const std::string& dir, const std::string& name){
+			std::string path(dir);
+			path.append(seprator);
+			path.append(name);
+			return path;
+		}
 	}
 };
 
